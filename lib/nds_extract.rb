20e0f3c6b3a9b602d18bf2c85e6e97d2f3213e96
Keenan Jones <keenanjones210@gmail.com>
2020-01-01 19:47:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,8 +49,8 @@ def total_gross(source)
   total_numb = 0
   director_name = list_of_directors(source)[i_directors]
   while i_directors < source.length do 
-    total_gross = gross_for_director(source[i_directors])
-    total_numb += directors_totals(source)[director_name]
+    #total_numb += 
+    puts directors_totals(source)[director_name]
     i_directors += 1
   end 
   total_numb
